c0c0582536134132d9d6d85bc9f610b1ac0ddb2a
DuyLinh0212 <tienben9123@gmail.com>
2026-03-30 10:35:56 +0700

    Change Config

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ config = {
     'starting_epoch' : 0,
     'batch_size' : 1,
     'log_val' : 10,
-    'task' : 'abnormal', # "meniscus" and  "acl" are the other options
-    'weight_decay' : 0.01,
+    'task' : 'acl', # "meniscus" and  "acl" are the other options
+    'weight_decay' : 1e-4,
     'patience' : 5,
     'save_model' : 1,
     'exp_name' : 'test',
     # Colab-friendly defaults to reduce GPU memory
     'image_size' : 192,
     'target_slices' : 24,
-    'num_workers' : 0
+    'num_workers' : 4
 }
