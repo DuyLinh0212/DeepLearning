aea6c74ef50cdccd14ab415587943eec55f5a93e
DuyLinh0212 <tienben9123@gmail.com>
2026-04-09 20:09:22 +0700

    Update config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ config = {
     'log_train' : 100,
     'lr' : 3e-5,
     'starting_epoch' : 0,
-    'batch_size' : 2,
+    'batch_size' : 4,
     'log_val' : 10,
     'tasks' : ['abnormal', 'acl', 'meniscus'],
     'task_name' : 'multilabel',
@@ -13,6 +13,6 @@ config = {
     'exp_name' : 'test',
     # Colab-friendly defaults to reduce GPU memory
     'image_size' : 224,
-    'target_slices' : 28,
+    'target_slices' : 32,
     'num_workers' : 3
 }
